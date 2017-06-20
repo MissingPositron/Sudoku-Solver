e83363e25f5f81a6d1b9676054e9e469752d001e
Chang Liu <MissingPositron@users.noreply.github.com>
2017-06-20 21:46:58 +0200

    some tools for imaging manipulation

--- source/tools.py
+++ source/tools.py
@@ -36,5 +36,70 @@ class Tools(object):
         return edges
 
 
+    def dilate(self,img, kernel):
+        cv2.dilate(img, kernel)
+        return img
+
+
+    def largestContour(self, image):
+        if self.isCV2():
+            contours, h = cv2.findContours(
+                img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        else:
+            _, contours, h = cv2.findContours(
+                img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        return max(contours, key = cv2.contourArea)
+
+
+    def largest4sideContour(self, img):
+        if self.isCV2():
+            contours, h = cv2.findContours(
+                img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        else:
+            _, contours, h = cv2.findContours(
+                img, cv2.RETR_TREE, cv2.CHAIN_APPROX_CHAIN)
+
+        contours = sorted(contours, key = cv2.ContourArea, reverse = True)
+        for cnt in contours[:min(5, len(contours))]:
+            im = img.copy()
+            cv2.drawContours(im, cnt, -1, (255, 255, 255), 5)
+            self.show(im, 'contour')
+
+            if len(self.approx(cnt)) == 4:
+                return cnt
+        return None
+
+
+    def make_it_square(self, img, length = 306):
+        return cv2.resize(img, (length, length))
+
+
+    def area(self, img):
+        return float(img.shape[0]*img.shape[1])
+
+
+    def cut_out_sudoku_puzzle(self, img, contour):
+        x, y, w, h = cv2.bondingRect(contour)
+        img = img[y:y+h, x:x+w]
+        return self.make_it_square(img, min(img.shape))
+
+
+    def binarized(self, img):
+        for i in xrange(img.shape[0]):
+            for j in xrange(img.shape[1]):
+                img[i][j] = 255*int(img[i][j]!=255)
+        return img
+
+
+    def approx(self, cnt):
+        peri = cv2.arcLength(cnt, True)
+        app = cv2.approxPolyDp(cnt, 0.01*peri, True)
+        return app
+
+
+
+
+
+
 
 
